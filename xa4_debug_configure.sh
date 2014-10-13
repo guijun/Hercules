009ea23db47429fbf213cb7d301dad27d31d4e86
kinba <kinba@sina.com>
2014-10-13 10:53:37 +0800

    enable debug for gdb

--- xa4_debug_configure.sh
+++ xa4_debug_configure.sh
@@ -1,2 +1,2 @@
 #/bin/sh
-./configure --enable-packetver=20140115 --enable-packetver-re --enable-buildbot=yes --enable-rdtsc --enable-debug=yes
+./configure --enable-packetver=20140115 --enable-packetver-re --enable-buildbot=yes --enable-rdtsc --enable-debug=gdb
